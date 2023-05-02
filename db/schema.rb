8a19a5736abc8be5f8882fbe352435694fa06870
nogu66 <ezochi66@gmail.com>
2023-05-02 14:04:11 +0900

    完成

--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_26_124340) do
   create_table "line_foods", force: :cascade do |t|
     t.integer "food_id", null: false
     t.integer "restaurant_id", null: false
-    t.integer "oreder_id"
+    t.integer "order_id"
     t.integer "count", default: 0, null: false
     t.boolean "active", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_line_foods_on_food_id"
-    t.index ["oreder_id"], name: "index_line_foods_on_oreder_id"
+    t.index ["order_id"], name: "index_line_foods_on_order_id"
     t.index ["restaurant_id"], name: "index_line_foods_on_restaurant_id"
   end
 
@@ -50,6 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_26_124340) do
 
   add_foreign_key "foods", "restaurants"
   add_foreign_key "line_foods", "foods"
-  add_foreign_key "line_foods", "oreders"
+  add_foreign_key "line_foods", "orders"
   add_foreign_key "line_foods", "restaurants"
 end
